02e4a360ef869512d1b3314478c117ec4d43e983
wangxingyu <102325 4018@qq.com>
2022-05-09 20:47:18 +0800

    init

--- src/store/modules/api.js
+++ src/store/modules/api.js
@@ -8,18 +8,12 @@ const api = {
     databaseUploadApi: baseUrl + '/api/database/upload',
     // 实时控制台
     socketApi: baseUrl + '/websocket?token=kl',
-    // 图片上传
-    imagesUploadApi: baseUrl + '/api/localStorage/pictures',
     // 修改头像
     updateAvatarApi: baseUrl + '/api/users/updateAvatar',
-    // 上传文件到七牛云
-    qiNiuUploadApi: baseUrl + '/api/qiNiuContent',
     // Sql 监控
     sqlApi: baseUrl + '/druid/index.html',
     // swagger
     swaggerApi: baseUrl + '/swagger-ui.html',
-    // 文件上传
-    fileUploadApi: baseUrl + '/api/localStorage',
     // baseUrl，
     baseApi: baseUrl
   }
